6a09b009656542cf1be0fe8ee1dacf9f819c56b1
Imran Said <imransaid247@gmail.com>
2019-04-12 16:18:29 +0300

    Initial Commit

---
+++ app/OrderMaster.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderMaster extends Model
+{
+    //
+    protected $table = 'order_master';
+    public $timestamps =false;
+}
---
+++ app/UOMOrderDetail.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UOMOrderDetail extends Model
+{
+    //
+    protected $table = 'uom_order_detail';
+    public $timestamps =false;
+}
